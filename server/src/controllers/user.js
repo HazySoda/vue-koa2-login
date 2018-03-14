6a9b58602eac4bf4f1b83d505f4c6710f3c7a6b5
KidneyFlower <753475777@qq.com>
2018-03-14 17:13:40 +0800

    feat: 用户登录时签发 token

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+const config = require('../config/index')
 const UserModel = require('../init/db').User
 
 module.exports = {
@@ -7,7 +9,18 @@ module.exports = {
         id: 0
       }
     })
-    ctx.body = user
+    // 要在 token 中附带的信息
+    const payload = {
+      uid: user.id,
+      phone: user.phone
+    }
+    // 签发 token 并返回给客户端
+    const token = jwt.sign(payload, config.jwt.secret, config.jwt.options)
+    ctx.status = 200
+    ctx.body = {
+      code: 200,
+      token
+    }
   },
   async reg (ctx, next) {
     ctx.body = 'Response for /user/reg'
