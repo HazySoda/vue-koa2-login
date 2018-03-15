0bb5bed76359809418b0a3ab7a17e03e278fdc44
KidneyFlower <753475777@qq.com>
2018-03-15 16:49:11 +0800

    feat: 完成用户注册逻辑

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,6 +1,23 @@
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const { regexs } = require('../util')
 const config = require('../config/index')
 const UserModel = require('../init/db').User
+
+// 校验请求参数
+const validate = params => {
+  if (typeof params !== 'object') {
+    throw new Error('Param must be a Object')
+  }
+  const keys = Object.keys(params)
+  if (!keys.length) {
+    return false
+  }
+  const valid = keys.every(key => {
+    return regexs[key].test(params[key])
+  })
+  return valid
+}
 
 module.exports = {
   async login (ctx, next) {
@@ -23,6 +40,56 @@ module.exports = {
     }
   },
   async reg (ctx, next) {
-    ctx.body = 'Response for /user/reg'
+    // 获取请求参数
+    const { phone, password, nickname } = ctx.request.body
+    // 校验请求参数
+    const valid = validate({phone, password, nickname})
+    if (!valid) {
+      ctx.status = 400
+      ctx.body = {
+        code: 400,
+        message: '参数错误'
+      }
+      return
+    }
+    try {
+      // 判断手机号码是否已经注册
+      const user = await UserModel.findOne({
+        where: {
+          phone
+        }
+      })
+      if (user) {
+        ctx.status = 400
+        ctx.body = {
+          code: 400,
+          message: '该手机号码已被注册'
+        }
+        return
+      }
+      // 加密明文密码
+      const hashPwd = await bcrypt.hash(password, 10)
+      // 构建用户数据
+      const data = {
+        phone,
+        password: hashPwd,
+        nickname
+      }
+      // 存入数据库
+      await UserModel.create(data)
+      ctx.status = 200
+      ctx.body = {
+        code: 200,
+        message: '注册成功',
+        data
+      }
+    } catch (e) {
+      console.log(e)
+      ctx.status = 500
+      ctx.body = {
+        code: 500,
+        message: e.message
+      }
+    }
   }
 }
