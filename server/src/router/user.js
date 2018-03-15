e2555389a14827609029a0b995eee52f40b64fc7
KidneyFlower <753475777@qq.com>
2018-03-15 18:01:15 +0800

    fix: 登录接口中不再校验 token

--- server/src/router/user.js
+++ server/src/router/user.js
@@ -1,10 +1,10 @@
 const router = require('koa-router')()
 const userController = require('../controllers/user')
-const auth = require('../middlewares/auth')
+// const auth = require('../middlewares/auth')
 
 router.prefix('/user')
 
-router.post('/login', auth, userController.login)
+router.post('/login', userController.login)
 
 router.post('/reg', userController.reg)
 
