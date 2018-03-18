eb9509ceab33582d251420d9e043418a8d64278b
KidneyFlower <753475777@qq.com>
2018-03-18 18:23:00 +0800

    chore: 移除已删除组件相关代码，增加简易 CSS Reset

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{name}}</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      name: 'Home'
+    }
   }
 }
 </script>
