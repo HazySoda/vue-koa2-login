83e29cff981e06c23f7f94ed011f0163f2553124
KidneyFlower <753475777@qq.com>
2018-03-19 15:43:27 +0800

    refactor: 根据规范来获取 Authorization 请求头

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -3,7 +3,8 @@ const config = require('../config/index')
 
 module.exports = async (ctx, next) => {
   // 从 Request Headers 的 Authorization 字段中获取 token
-  const token = ctx.get('Authorization')
+  const authorization = ctx.get('Authorization')
+  const token = authorization.split(' ')[1]
   // 如果获取不到 token，则抛出错误
   if (!token) {
     ctx.status = 401
