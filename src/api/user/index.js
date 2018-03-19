c9c637334074503e07804fbedf2171ff92015725
KidneyFlower <753475777@qq.com>
2018-03-19 15:48:05 +0800

    feat: 增加了获取用户状态的接口配置

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -5,3 +5,6 @@ export const login = data => _axios.post('/user/login', data)
 
 // 用户注册
 export const reg = data => _axios.post('/user/reg', data)
+
+// 获取用户状态
+export const getUserStatus = () => _axios.get('/user/status')
