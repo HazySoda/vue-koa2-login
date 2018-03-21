629cbd8b0d0fdfb62f2948e6d2ef888c68d6bc08
KidneyFlower <753475777@qq.com>
2018-03-21 15:51:26 +0800

    feat: 前端项目中增加常用的正则表达式

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,9 @@
+const regexs = {
+  phone: /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/,
+  password: /^[A-Za-z][0-9A-Za-z]{5,17}$/,
+  nickname: /^[0-9a-zA-Z\u4e00-\u9fa5_]{2,10}$/
+}
+
 const getDateTime = timestamp => {
   const date = timestamp ? new Date(timestamp) : new Date()
   const prefix = (num) => num >= 10 ? num : `0${num}`
@@ -13,5 +19,6 @@ const getDateTime = timestamp => {
 }
 
 export {
+  regexs,
   getDateTime
 }
