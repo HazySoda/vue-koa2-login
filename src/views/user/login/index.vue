ed04237000a2293418b025a5ac0e329a3f8cab6b
KidneyFlower <753475777@qq.com>
2018-03-21 17:34:19 +0800

    feat: 完成登录页面表单验证

--- src/views/user/login/index.vue
+++ src/views/user/login/index.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="login">
     <div class="login-form__wrap">
-      <h1>Vue-Koa2-Login</h1>
-      <el-form :model="loginForm" label-width="55px">
-        <el-form-item label="手机号">
-          <el-input v-model="loginForm.phone" type="text" size="medium" autofocus clearable auto-complete="off"></el-input>
+      <h1>登录</h1>
+      <el-form size="medium" :model="loginForm" ref="loginForm" :rules="loginFormRules" label-width="65px">
+        <el-form-item label="手机号" prop="phone">
+          <el-input v-model="loginForm.phone" type="text" autofocus clearable auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
-          <el-input v-model="loginForm.password" type="password" size="medium" clearable auto-complete="off"></el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="loginForm.password" type="password" clearable auto-complete="off"></el-input>
         </el-form-item>
       </el-form>
-      <el-button type="primary" @click="submit">登录</el-button>
-      <el-button @click="goReg">注册新账号</el-button>
+      <div class="login-form__actions">
+        <el-button type="primary" @click="submit">登录</el-button>
+        <el-button @click="goReg">注册新账号</el-button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { regexs } from '@/util'
 import * as api from '@/api/user'
 
 export default {
@@ -25,23 +28,35 @@ export default {
       loginForm: {
         phone: '',
         password: ''
+      },
+      loginFormRules: {
+        phone: [
+          {required: true, message: '请输入手机号码'},
+          {pattern: regexs.phone, message: '手机号码格式有误，请检查'}
+        ],
+        password: [
+          {required: true, message: '请输入密码'},
+          {pattern: regexs.password, message: '请输入6-18位字母数字组合 (第一位必须为字母)'}
+        ]
       }
     }
   },
   methods: {
     // 提交登录表单
-    async submit () {
-      const data = {
-        ...this.loginForm
-      }
-      try {
-        const res = await api.login(data)
-        localStorage.token = res.data.token
-        // 此处使用 replace，防止用户登陆成功后点击后退按钮重复登录
-        this.$router.replace('/')
-      } catch (err) {
-        console.log(err)
-      }
+    submit () {
+      this.$refs.loginForm.validate(async valid => {
+        if (valid) {
+          const data = {...this.loginForm}
+          try {
+            const res = await api.login(data)
+            localStorage.token = res.data.token
+            // 此处使用 replace，防止用户登陆成功后点击后退按钮重复登录
+            this.$router.replace('/')
+          } catch (err) {
+            console.log(err)
+          }
+        }
+      })
     },
     // 跳转到注册页面
     goReg () {
