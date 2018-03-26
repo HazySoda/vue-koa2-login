eedc502f9f0cd95dd7c390c9efd8b29956792a36
KidneyFlower <753475777@qq.com>
2018-03-26 18:01:28 +0800

    chore: 为配置项添加必要的注释

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,17 +1,17 @@
 module.exports = {
   db: {
-    dialect: 'mysql',
-    host: 'localhost',
-    port: '3306',
-    username: 'root',
-    password: 'password',
-    database: 'test',
+    dialect: 'mysql', // 连接数据库的类型
+    host: 'localhost', // MySQL 地址
+    port: '3306', // MySQL 端口号
+    username: 'root', // MySQL 用户名
+    password: 'password', // MySQL 密码
+    database: 'test', // 使用的数据库名称
     operatorsAliases: false // Issue #8417
   },
   jwt: {
-    secret: 'KidneyFlower',
+    secret: 'KidneyFlower', // jsonwebtoken 使用的 secret
     options: {
-      expiresIn: '60s'
+      expiresIn: '60s' // Token 的过期时间
     }
   }
 }
